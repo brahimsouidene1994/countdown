7e120e401d8b4aaaf403cbefc59f7ae9c706c376
brahimsouidene1994 <72886850+brahimsouidene1994@users.noreply.github.com>
2021-08-12 15:35:15 +0100

    test

--- src/component/coundown.js
+++ src/component/coundown.js
@@ -13,7 +13,7 @@ class Countdown extends React.Component {
         this.countdown = this.countdown.bind(this);
     }
     countdown = () =>{
-        const countDate = new Date("may 17, 2021 00:00:00").getTime();
+        const countDate = new Date("septembre 17, 2021 00:00:00").getTime();
         const now = new Date().getTime();
         const gap = countDate - now;
 
